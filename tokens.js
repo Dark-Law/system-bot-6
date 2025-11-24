0a5da9acadba38edbd54665433be79716c31848c
Dark-Law <kkulshi12@gmail.com>
2025-11-24 20:55:41 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,4 +1,4 @@
 export const config = {
   token: process.env.DISCORD_TOKEN || 'MTQ0MjIzNDEwODU2OTg0NTk2NA.G8YdL1.M09e6nzZzcgd7rs8JX3oPloRN_rcNUjEMpCjgE',
-  clientId: process.env.CLIENT_ID || 'https://discord.com/api/oauth2/authorize?client_id=1442234108569845964&permissions=8&scope=bot%20applications.commands',
+  clientId: process.env.CLIENT_ID || '1442234108569845964',
 };
